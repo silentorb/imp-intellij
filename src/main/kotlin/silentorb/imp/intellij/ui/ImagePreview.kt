2b77258b4f00fa4061d8e7831a22bdcc15a42786
cj <christopherwjohnson@gmail.com>
2020-03-02 11:27:21 -0700

    Added image preview tiling.

--- src/main/kotlin/silentorb/imp/intellij/ui/ImagePreview.kt
+++ src/main/kotlin/silentorb/imp/intellij/ui/ImagePreview.kt
@@ -2,17 +2,15 @@ package silentorb.imp.intellij.ui
 
 import com.intellij.util.ui.ImageUtil
 import silentorb.mythic.imaging.Bitmap
-import silentorb.mythic.spatial.Vector2i
+import java.awt.GridLayout
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_3BYTE_BGR
 import java.awt.image.BufferedImage.TYPE_BYTE_GRAY
-import java.io.ByteArrayInputStream
-import java.nio.ByteBuffer
-import java.nio.FloatBuffer
-import javax.imageio.ImageIO
 import javax.swing.ImageIcon
 import javax.swing.JComponent
 import javax.swing.JLabel
+import javax.swing.JPanel
 
 fun newImage(bitmap: Bitmap): BufferedImage {
   val buffer = bitmap.buffer
@@ -32,12 +30,24 @@ fun newImage(bitmap: Bitmap): BufferedImage {
   return image
 }
 
-fun newImagePreview(image: BufferedImage): JComponent {
+fun newImagePreview(image: Image): JComponent {
   return JLabel(ImageIcon(image))
 }
 
-fun newImagePreview(bitmap: Bitmap): JComponent {
-//    val image = newImage(bitmap.dimensions, rgbFloatToBytes(bitmap.buffer))
-  val image = newImage(bitmap)
-  return newImagePreview(image)
+fun newImagePreview(bitmap: Bitmap, tiled: Boolean = true): JComponent {
+  val fullImage = newImage(bitmap)
+  return if (tiled) {
+    val dimensions = bitmap.dimensions
+    val image = fullImage.getScaledInstance(dimensions.x / 3, dimensions.y / 3, Image.SCALE_DEFAULT)
+    val grid = GridLayout(3, 3)
+    val panel = JPanel()
+    panel.layout = grid
+    for (i in 0 until 9) {
+      panel.add(newImagePreview(image))
+    }
+    panel
+  }
+  else {
+    newImagePreview(fullImage)
+  }
 }
